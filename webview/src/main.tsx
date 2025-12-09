ad4ea81b935f94798611c225796837392e4d1aab
zkpaiminmin <270750933@qq.com>
2025-12-09 14:45:19 +0800

    fix:拆分app.css样式文件

--- webview/src/main.tsx
+++ webview/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './codicon.css';
-import './styles/app.css';
+import './styles/app.less';
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <App />,
