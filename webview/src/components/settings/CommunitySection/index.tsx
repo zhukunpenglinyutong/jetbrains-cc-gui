5e2fe918f254cfee014c2f7d4852ad777a5549c6
zkpaiminmin <270750933@qq.com>
2026-02-19 20:22:46 +0800

    chore: bump version to 0.2 and remove sponsor section
    - Update version from 0.1.9-fix to 0.2 in build.gradle
    - Remove sponsor QR codes (Alipay, WeChat, PayPal) from community settings
    - Clean up related styles and unused imports

--- webview/src/components/settings/CommunitySection/index.tsx
+++ webview/src/components/settings/CommunitySection/index.tsx
@@ -5,32 +5,8 @@ import { CHANGELOG_DATA } from '../../../version/changelog';
 import styles from './style.module.less';
 
 const CommunitySection = () => {
-  const { t, i18n } = useTranslation();
-  const isSimplifiedChinese = i18n.language === 'zh';
+  const { t } = useTranslation();
   const [showChangelog, setShowChangelog] = useState(false);
-
-  const sponsorItems = [
-    {
-      key: 'alipay',
-      src: 'https://claudecodecn-1253302184.cos.ap-beijing.myqcloud.com/zfb.jpg',
-      label: t('settings.alipay'),
-    },
-    {
-      key: 'wechat',
-      src: 'https://claudecodecn-1253302184.cos.ap-beijing.myqcloud.com/wx.jpg',
-      label: t('settings.wechatPay'),
-    },
-    {
-      key: 'paypal',
-      src: 'https://claudecodecn-1253302184.cos.ap-beijing.myqcloud.com/paypal.png',
-      label: t('settings.paypal'),
-    },
-  ];
-
-  // For non-Simplified Chinese locales, put PayPal first
-  const orderedItems = isSimplifiedChinese
-    ? sponsorItems
-    : [sponsorItems[2], sponsorItems[0], sponsorItems[1]];
 
   return (
     <div className={styles.configSection}>
@@ -60,25 +36,6 @@ const CommunitySection = () => {
           <span className="codicon codicon-history" />
           {t('settings.versionHistory')}
         </button>
-      </div>
-
-      {/* Sponsorship support */}
-      <div className={styles.sponsorSection}>
-        <h3 className={styles.sectionTitle}>{t('settings.sponsor')}</h3>
-        <p className={styles.sectionDesc}>{t('settings.sponsorDesc')}</p>
-
-        <div className={styles.sponsorContainer}>
-          {orderedItems.map((item) => (
-            <div key={item.key} className={styles.sponsorItem}>
-              <img
-                src={item.src}
-                alt={item.label}
-                className={styles.sponsorImage}
-              />
-              <p className={styles.sponsorLabel}>{item.label}</p>
-            </div>
-          ))}
-        </div>
       </div>
 
       <ChangelogDialog
